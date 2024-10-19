3dca79c5fef3369f0bc1ae5a66715ddf42c1281c
{Osamashehab428} <{shehabosama983@gmail.com}>
2024-10-20 00:58:37 +0300

    Message

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,17 @@
 def  perform_operations(num1 = float(input()),
                           num2 = float(input()),
                           operation = input("+","-","*","/")):
+    print("Arithmetic operation")
+    num1 = float(input())
+    num2 = float(input())
     if operation == "+":
         print(num1+num2)
+    elif operation == "-":
+        print(num1 - num2)
+    elif operation == "*":
+        print(num1 * num2)
+    elif operation == "/":
+        print(num1/num2)
 
 
 
