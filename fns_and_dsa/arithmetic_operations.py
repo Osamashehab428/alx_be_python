07fd9d0dec977d8592ba4f0a0185ceb8844a3da5
{Osamashehab428} <{shehabosama983@gmail.com}>
2024-10-20 00:54:35 +0300

    folder

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,22 +1,20 @@
+def  perform_operations(num1 = float(input()),
+                          num2 = float(input()),
+                          operation = input("+","-","*","/")):
+    if operation == "+":
+        print(num1+num2)
+
+
+
+perform_operations()
+
  
-def perform_operation():
-    print("Enter the first number")
-    num1 = float(input())
-    print("Enter the second number")
-    num2 = float(input())
-    print("Enter the operation (add, subtract, multiply, divide):")
-    operation = input()
-    if(operation == '+'):
-        print("Result: ",num1+num2)
-    elif(operation == '-'):
-        print("Result: ",num1-num2)
-    elif(operation == '*'):
-        print("Result: ",num1*num2)
-    elif(operation == '/' and num1 !=0 and num2 !=0):            
-        print("Result: ",num1/num2)
-    elif(operation == '/'and num2 ==0):
-        print("Please enter a valid number \"the second value shouldn't equal zero\"")
 
 
-perform_operation()
+
+
+
+
+
+
 
