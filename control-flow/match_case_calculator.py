54ce0a25c9a1ed1fdc4e53c6cd6ad5c9ed42c9e2
{Osamashehab428} <{shehabosama983@gmail.com}>
2024-10-28 02:06:23 +0300

    match case calculator adjustments

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,10 +4,10 @@ num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
-    case "+": print("result = {num1 + num2}")
-    case "-": print("result = {num1 - num2}")
-    case "*": print("result = {num1 * num2}")
-    case "/": print("result = {num1 / num2}")
+    case "+": print(f"result = {num1 + num2}")
+    case "-": print(f"result = {num1 - num2}")
+    case "*": print(f"result = {num1 * num2}")
+    case "/": print(f"result = {num1 / num2}")
 
 
 
