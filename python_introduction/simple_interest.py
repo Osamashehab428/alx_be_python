38b3654538bf8ebb11a27c494da300259c79c0cd
{Osamashehab428} <{shehabosama983@gmail.com}>
2024-10-27 23:49:45 +0300

    basic

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,9 +2,9 @@ principal = 1000
 rate = 0.05
 time = 3
 
-I = principal*rate*time
+interest = principal*rate*time
 
-print("The simple interest is:",I)
+print("The simple interest is:",interest)
 
 
 
